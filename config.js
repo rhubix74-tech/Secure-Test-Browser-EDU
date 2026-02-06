a9338f672f8d6ee2c616db0d8673ac3199d3dbec
rhubix74-tech <rhubix74@gmail.com>
2026-02-05 21:18:13 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
   challenge: true, // Set to true if you want to enable password protection.
-  users: {
+  users: {mister 64
     // You can add multiple users by doing username: 'password'.
     interstellar: "bananas",
   },
